466e6f394411abee8505618ad1a4e20ae7dbe876
Pedro Gomes <pedromvgomes@hotmail.com>
2024-09-09 16:40:58 +0100

    feat: new getOrSet map extension (#11)
    * feat: new getOrPut map extension
    * bump eslint to 9.10

--- packages/extensions/tests/map.extensions.test.ts
+++ packages/extensions/tests/map.extensions.test.ts
@@ -7,4 +7,21 @@ describe('Subject: Map extensions', () => {
     const mapped = map.map((value, key) => `${key} = ${value}`);
     expect(mapped).toEqual(["One = 1", "Two = 2", "Three = 3"]);
   });
+
+  it ('Scenario 02: Using getOrSet method I am able to get the value for a key or set it if it does not exist', async () => {
+    const map = new Map<string, number>();
+    const value = map.getOrSet("One", () => 1);
+    expect(value).toBe(1);
+    expect(map.get("One")).toBe(1);
+  });
+
+  it ('Scenario 03: Using getOrSet method If a value exists my factory method is not called', async () => {
+    const map = new Map<string, number>();
+    map.set("One", 1);
+    const func = jest.fn();
+    const value = map.getOrSet("One", func);
+
+    expect(value).toBe(1);
+    expect(func).not.toHaveBeenCalled();
+  });
 });
