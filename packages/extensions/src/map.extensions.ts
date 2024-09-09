4ea0a4e0692f37a16687fe9941bbdfd634dbd19c
pedromvgomes <pedromvgomes@hotmail.com>
2024-09-09 16:38:35 +0100

    feat: new getOrPut map extension

--- packages/extensions/src/map.extensions.ts
+++ packages/extensions/src/map.extensions.ts
@@ -1,5 +1,7 @@
 declare interface Map<K, V> {
   map<T>(callbackfn: (value: V, key: K) => T): T[]
+
+  getOrSet(key: K, factory: (key: K) => V): V
 }
 
 Map.prototype.map = function <K, V, T>(this: Map<K, V>, callbackfn: (value: V, key: K) => T): T[] {
@@ -8,4 +10,13 @@ Map.prototype.map = function <K, V, T>(this: Map<K, V>, callbackfn: (value: V, k
     result.push(callbackfn(value, key))
   })
   return result
+}
+
+Map.prototype.getOrSet = function <K, V>(this: Map<K, V>, key: K, factory: (key: K) => V): V {
+  let value = this.get(key)
+  if (value === undefined) {
+    value = factory(key)
+    this.set(key, value)
+  }
+  return value
 }
